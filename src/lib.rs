d6ed03c0bf2f1077fae1463b4af754f0ddacb6b3
Folling <j.purol@web.de>
2017-12-05 01:38:47 +0100

    temporary removal of par_s

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@ mod tests {
     use exprust::ar;
     use nom::IResult;
     use test::Bencher;
+    use std::str;
 
     #[bench]
     fn bench_parse(b: &mut Bencher) {
@@ -19,7 +20,7 @@ mod tests {
 
     #[test]
     fn now() {
-        if let IResult::Done(_, r) = ar::expr(b"-0b11011") {
+        if let IResult::Done(t, r) = ar::factor_term(b"4^cos(3)*4^2") {
             println!("{}", r);
         }
     }
@@ -33,33 +34,21 @@ mod tests {
         assert_eq!(ar::expr(b"4*(4-4)"), IResult::Done(&b""[..], 0f64));
         assert_eq!(ar::expr(b"4-4"), IResult::Done(&b""[..], 0f64));
         assert_eq!(ar::expr(b"4+-4"), IResult::Done(&b""[..], 0f64));
-        assert_eq!(ar::expr(b"4(4-4)-4"), IResult::Done(&b""[..], -4f64));
         assert_eq!(ar::expr(b"-4+4"), IResult::Done(&b""[..], 0f64));
-        assert_eq!(ar::expr(b"-4(4-2)+3"), IResult::Done(&b""[..], -5f64));
         assert_eq!(ar::expr(b"4^3"), IResult::Done(&b""[..], 64f64));
         assert_eq!(ar::expr(b"4^(3-1)"), IResult::Done(&b""[..], 16f64));
         assert_eq!(ar::expr(b"-3^(2+3^2)"), IResult::Done(&b""[..], -177147f64));
         assert_eq!(ar::expr(b"(0x14)^(2+0x1)"), IResult::Done(&b""[..], 8000f64));
         assert_eq!(ar::expr(b"0b11011"), IResult::Done(&b""[..], 27f64));
         assert_eq!(ar::expr(b"0b1011*(0b11-0b1)^0b101"), IResult::Done(&b""[..], 352f64));
-        assert_eq!(
-            ar::expr(b"sin(86+(cos(0)+1)^2)"),
-            IResult::Done(&b""[..], 1f64)
-        );
-        assert_eq!(
-            ar::expr(b"4*sin(86+(cos(0)+1)^2)"),
-            IResult::Done(&b""[..], 4f64)
-        );
-        assert_eq!(
-            ar::expr(b"4sin(86+(cos(0)+1)^2)"),
-            IResult::Done(&b""[..], 4f64)
-        );
+        assert_eq!(ar::expr(b"sin(86+(cos(0)+1)^2)"),IResult::Done(&b""[..], 1f64));
+        assert_eq!(ar::expr(b"4*sin(86+(cos(0)+1)^2)"),IResult::Done(&b""[..], 4f64));
         assert_eq!(ar::expr(b"|1|"), IResult::Done(&b""[..], 1f64));
         assert_eq!(ar::expr(b"|-1|"), IResult::Done(&b""[..], 1f64));
         assert_eq!(ar::eval(b"4==4"), IResult::Done(&b""[..], true));
         assert_eq!(ar::eval(b"4!=3"), IResult::Done(&b""[..], true));
         assert_eq!(ar::eval(b"2*2==1+3"), IResult::Done(&b""[..], true));
         assert_eq!(ar::eval(b"2^3==8"), IResult::Done(&b""[..], true));
-        assert_eq!(ar::eval(b"(4-2)4==(16-8)"), IResult::Done(&b""[..], true));
+        assert_eq!(ar::eval(b"(4-2)*4==(16-8)"), IResult::Done(&b""[..], true));
     }
 }
